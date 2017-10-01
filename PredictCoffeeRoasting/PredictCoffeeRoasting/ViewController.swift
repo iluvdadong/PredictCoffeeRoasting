f4b126e74bbe3b5566dbe5464d0b5804ffbe6f1f
iluvdadong <iluvdadong@naver.com>
2017-10-01 18:11:28 +0900

    update

---
+++ PredictCoffeeRoasting/PredictCoffeeRoasting/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PredictCoffeeRoasting
+//
+//  Created by 맥동이 on 2017. 10. 1..
+//  Copyright © 2017년 donggukVision. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
