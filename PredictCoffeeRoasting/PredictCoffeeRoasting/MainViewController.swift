cce6562c1988295e37d467a89d1674cbd2d36d6d
iluvdadong <iluvdadong@naver.com>
2017-11-09 02:06:33 +0900

    hex Value to RGB from User Environment

--- PredictCoffeeRoasting/PredictCoffeeRoasting/MainViewController.swift
+++ PredictCoffeeRoasting/PredictCoffeeRoasting/MainViewController.swift
@@ -90,8 +90,13 @@ UINavigationControllerDelegate {
             color = UIColor(red: CGFloat(pixel[0]) / 255.0, green: CGFloat(pixel[1]) / 255.0, blue: CGFloat(pixel[2]) / 255.0, alpha: CGFloat(pixel[3]) / 255.0)
         }
         
+        
         if color != nil {
             print( self.toHexString(color: color!) )
+            var hexValue:String = self.toHexString(color: color!)
+            print(self.hexStringToUIColor(hex: hexValue ))
+     
+        
         }
         pixel.deallocate(capacity: 4)
     }
@@ -108,7 +113,49 @@ UINavigationControllerDelegate {
         
         return String(format:"#%06x", rgb)
     }
-   
+    
+    
+    func hexStringToUIColor (hex:String) -> UIColor {
+        var cString:String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
+        
+        if (cString.hasPrefix("#")) {
+            cString.remove(at: cString.startIndex)
+        }
+        
+        if ((cString.characters.count) != 6) {
+            return UIColor.gray
+        }
+        
+        var rgbValue:UInt32 = 0
+        Scanner(string: cString).scanHexInt32(&rgbValue)
+        
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
+    }
+//        
+//        if color != nil {
+//            print( self.toHexString(color: color!) )
+//        }
+//        pixel.deallocate(capacity: 4)
+//    }
+//    
+//    func toHexString(color: UIColor) -> String {
+//        var r:CGFloat = 0
+//        var g:CGFloat = 0
+//        var b:CGFloat = 0
+//        var a:CGFloat = 0
+//        
+//        color.getRed(&r, green: &g, blue: &b, alpha: &a)
+//        
+//        let rgb:Int = (Int)(r*255)<<16 | (Int)(g*255)<<8 | (Int)(b*255)<<0
+//        
+//        return String(format:"#%06x", rgb)
+//    }
+//   
     
 }
 
